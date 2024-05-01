60a764d938b4e56e9e125ef12d5051011ef27893
Juanrianoq <juanriano0523@gmail.com>
2024-05-01 01:24:44 -0500

    Add main style for news layer.

--- index.php
+++ index.php
@@ -3,17 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrate</title>
+    <title>GameRevolt | Inicio</title>
     <link rel="stylesheet" href="assets/css/layout.css">
+    <link rel="stylesheet" href="assets/css/main.css">
     <link rel="icon" href="assets/images/control.ico">
 </head>
 <body>
+    <?php
+        require 'views/layout/header.php';
+    ?>
+
+    <main>
+        <h1>main</h1>
+    </main>
 </body>
 </html>
-
-<?php
-
-    include 'views/layout/header.php';
-    include 'views/layout/footer.php'
-
-?>
--- views/layout/header.php
+++ views/layout/header.php
@@ -1,26 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-    <body>
-        <header>
-            <div class="logo">
-                <img src="assets/images/control.png" class="control" alt="logo-imagen">
-                <h2 class="logo-nombre">GameRevolt</h2>
-            </div>
-            <div class="barra">
-                <div class="botones-base">
-                    <nav>
-                        <a href="#" class="nav-inicio">INICIO</a>
-                        <a href="#" class="nav-consejos">CONSEJOS Y TUTORIALES</a>
-                        <a href="#" class="nav-ranking">RANKING</a>
-                        <a href="#" class="nav-reseñas">RESEÑAS</a>
-                    </nav>
-                </div>
-                <div class="iniciar-sesion">
-                    <a href="#">
-                        <img src="assets/images/icon-user.png" class="user" alt="icono-user">
-                    </a>
-                </div>
-            </div>
-        </header>
-    </body>
-</html>
+<header>
+    <div class="logo">
+        <img src="assets/images/control.png" class="control" alt="logo-imagen">
+        <h2 class="logo-nombre">GameRevolt</h2>
+    </div>
+    <div class="barra">
+        <div class="botones-base">
+            <nav>
+                <a href="#" class="nav-inicio">INICIO</a>
+                <a href="#" class="nav-consejos">CONSEJOS Y TUTORIALES</a>
+                <a href="#" class="nav-ranking">RANKING</a>
+                <a href="#" class="nav-reseñas">RESEÑAS</a>
+            </nav>
+        </div>
+        <div class="iniciar-sesion">
+            <a href="#">
+                <img src="assets/images/icon-user.png" class="user" alt="icono-user">
+            </a>
+        </div>
+    </div>
+</header>
