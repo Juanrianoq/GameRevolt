7f3e3b90ea48a0853b6f365d9ca7737a12bd3b9d
Juanrianoq <juanriano0523@gmail.com>
2024-05-01 17:57:40 -0500

    Add news 1

--- index.php
+++ index.php
@@ -14,7 +14,29 @@
     ?>
 
     <main>
-        <h1>main</h1>
+        <div id="news-container">
+        <h1 id="title">Noticias</h1>
+            <article class="news">
+                <a href="#">
+
+                    <img src="assets/images/noticias/Imagen-Noticia1.jpeg" alt="Captura del armamento y vehiculos pesados de Call of Duty: Cold War" class="images">
+
+                    <div class="text-container">
+                        <h2 class="art-title">
+                            Activision ha revelado el futuro de la saga Call of Duty por culpa de un descuido en su última actualización. Un nuevo Black Ops podría confirmarse dentro de muy poco
+                        </h2>
+
+                        <h3 class="art-description">
+                            Multitud de pistas apuntan a que una sexta entrega de Call of Duty: Black Ops está a punto de hacerse realidad
+                        </h3>
+
+                        <h4 class="autor">
+                            por Bruno Ouviña el 23 de abril de 2024 a las 10:08 a.m.
+                        </h4>
+                    </div>   
+                </a>
+            </article>
+        </div>
     </main>
 </body>
 </html>
