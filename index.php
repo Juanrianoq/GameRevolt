b32a3435b19c824a6a0ad581678dbebb34e6fdd7
Juanrianoq <juanriano0523@gmail.com>
2024-05-02 15:53:34 -0500

    Doing main responsive.

--- index.php
+++ index.php
@@ -17,15 +17,16 @@
         <div id="news-container">
         <h1 id="title">Noticias</h1>
             <article class="news">
+
                 <a href="#">
-
                     <img src="assets/images/noticias/Imagen-Noticia1.jpeg" alt="Captura del armamento y vehiculos pesados de Call of Duty: Cold War" class="images">
-
+                </a>
+                <a href="#">
                     <div class="text-container">
                         <h2 class="art-title">
                             Activision ha revelado el futuro de la saga Call of Duty por culpa de un descuido en su última actualización. Un nuevo Black Ops podría confirmarse dentro de muy poco
                         </h2>
-
+                        <br>
                         <h3 class="art-description">
                             Multitud de pistas apuntan a que una sexta entrega de Call of Duty: Black Ops está a punto de hacerse realidad
                         </h3>
@@ -36,7 +37,33 @@
                     </div>   
                 </a>
             </article>
+
+            <hr>
+
+            <article class="news">
+                <a href="#">
+                    <img src="assets/images/noticias/Imagen-Noticia2.jpeg" alt="Imagen de un PS4 y un XBOX siendo comparados" class="images">
+                </a>
+                <a href="#">
+                    <div class="text-container">
+                        <h2 class="art-title">
+                            "La demanda es muy débil". Las ventas de PS5 y Xbox Series X|S no contentan ni a AMD este 2024, que estima una caída importante para el resto del año
+                        </h2>
+                        <br>
+                        <h3 class="art-description">
+                            AMD dice que en la segunda mitad de 2024 será peor que la primera
+                        </h3>
+
+                        <h4 class="autor">
+                            por Adrián Mira el 22 de abril de 2024 a las 13:25 p.m.
+                        </h4>
+                    </div>   
+                </a>
+            </article>
         </div>
     </main>
+    <?php
+        require 'views/layout/footer.php';
+    ?>
 </body>
 </html>
---
+++ views/ranking.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GameRevolt</title>
+  <link rel="stylesheet" href="estilo_cs.css">
+</head>
+<body>
+  <header>
+    <div class="logo">
+      <img src="imagenes/control.png" class="control" alt="logo-imagen">
+      <h2 class="logo-nombre">GameRevolt</h2>
+    </div>
+    <div class="barra">
+      <div class="botones-base">
+        <nav>
+          <a href="#" class="nav-inicio">INICIO</a>
+          <a href="#" class="nav-consejos">CONSEJOS Y TUTORIALES</a>
+          <a href="#" class="nav-ranking">RANKING</a>
+          <a href="#" class="nav-reseñas">RESEÑAS</a>
+        </nav>
+      </div>
+      <div class="iniciar-sesion">
+        <a href="#">
+          <img src="imagenes/icon-user.png" class="user" alt="icono-user">
+        </a>
+      </div>
+    </div>
+  </header>
+  <main>
+    <h2>Top 6: Los Mejores Juegos</h2>
+    <div class="table-box">
+      <table class="top-games-table">
+        <thead>
+          <tr>
+            <th>Posición</th>
+            <th>Juego y plataforma</th>
+            <th>Género</th>
+            <th>Estreno</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td> 1 <img src="imagenes/fifa23.png" width="100"></td>
+            <td>FIFA 23 - PS5</td>
+            <td>Deportes</td>
+            <td>30-09-2022</td>
+          </tr>
+          <tr>
+            <td>  2   <img src="imagenes/elysium.png" width="100"> </td>
+            <td>Valkyrie Elysium - P54</td>
+            <td>Rol</td>
+            <td>29-09-2022</td>
+          </tr>
+          <tr>
+            <td>  3   <img src="imagenes/strayblade.png" width="100"></td>
+            <td>Stray Blade - Xbox Series</td>
+            <td>Acción</td>
+            <td>20-04-2023</td>
+          </tr>
+          <tr>
+            <td>  4   <img src="imagenes/manorlords.png" width="100"></td>
+            <td>Manor Lords - PC</td>
+            <td>Estrategia</td>
+            <td>26-04-2024</td>
+          </tr>
+          <tr>
+            <td>  5   <img src="imagenes/gtaV.png" width="100"></td>
+            <td>Grand Theft Auto VI - Xbox Series</td>
+            <td>Acción</td>
+            <td>Por determinar</td>
+          </tr>
+          <tr>
+            <td>  6   <img src="imagenes/miasma.png" width="100"></td>
+            <td>Miasma Chronicles - PS5</td>
+            <td>Rol</td>
+            <td>23-05-2023</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </main>
+  <footer>
+    <div class="logo">
+      <h2 class="logo-nombre">GameRevolt</h2>
+    </div>
+    <div class="nombres">
+      <h3>
+        Melanny Herrera | Juan Esteban | Elvis Montoya | Juan Angel
+      </h3>
+    </div>
+    <div class="copyright">
+      <a href="#" class="copy">&copy; 2024 www.GameRevolt.com</a>
+    </div>
+  </footer>
+</body>
+</html>
